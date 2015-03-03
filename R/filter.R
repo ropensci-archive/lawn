4ce048a89019fc2d1974a246f561df28547d5d5a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-02 22:15:18 -0800

    added filter fxn

---
+++ R/filter.R
@@ -0,0 +1,18 @@
+#' Calculate the distance between two points
+#'
+#' @export
+#'
+#' @param features A FeatureCollection
+#' @param key (character) The property on which to filter
+#' @param value (character) The value of that property on which to filter
+#' @examples
+#' dat <- filter_features
+#' filter(dat, 'species', 'oak')
+#' filter(dat, 'species', 'maple')
+#' filter(dat, 'species', 'redwood')
+filter <- function(features, key, value) {
+  ct$eval(sprintf('var key = "%s";', key))
+  ct$eval(sprintf('var value = "%s";', value))
+  ct$eval(sprintf("var filtered = turf.filter(%s, key, value);", features))
+  ct$get("filtered")
+}
