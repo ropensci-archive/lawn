e70d8376a8eb76338ddb5091220fcb4b3549eb15
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-03-04 16:12:36 -0500

    added buffer.R

---
+++ R/buffer.R
@@ -0,0 +1,38 @@
+#' Buffer a feature
+#'
+#' @export
+#' @param input A Feature or FeatureCollection
+#' @param dist distance used to buffer the input
+#' @param units (character) Can be miles, feet, kilometers, meters, or degrees
+#' @details Calculates and returns a buffered feature.
+#'
+#' @examples
+#' # From a FeatureCollection
+#' dat <- random(100)
+#' buffer(dat, 100)
+#'
+#' # From a Feature
+#' dat <- '{
+#'  "type": "Feature",
+#'  "properties": {},
+#'  "geometry": {
+#'      "type": "Polygon",
+#'      "coordinates": [[
+#'        [-112.072391,46.586591],
+#'        [-112.072391,46.61761],
+#'        [-112.028102,46.61761],
+#'        [-112.028102,46.586591],
+#'        [-112.072391,46.586591]
+#'      ]]
+#'    }
+#' }'
+#' buffer(dat, 1,"miles")
+
+buffer <- function(input,dist,units=c("meters", "feet", "kilometers", "miles", "degrees")) {
+  input<-convert(input)
+  units<-match.arg(units)
+  ct$eval(sprintf("var units = '%s';",units))
+  ct$eval(sprintf('var dist = %s;', dist))
+  ct$eval(sprintf("var buff = turf.buffer(%s, dist, units);", input))
+  ct$get("buff")
+}
