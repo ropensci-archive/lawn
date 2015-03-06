bdf1236a573dac85a4008f141b0ae5ff7b4dad94
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-05 21:50:02 -0800

    changed view fxn to accept character string or list, #3, #4 this fxn will change as we make each function accept either a list or character string

--- R/view.R
+++ R/view.R
@@ -1,15 +1,36 @@
-#' Visualize
+#' Visualize geojson from a character string or list
 #'
 #' @import leaflet
 #' @export
-#' @param x Input, a geojson character string
+#' @param x Input, a geojson character string or list
+#' @return Opens a map with the geojson object(s)
 #' @examples \dontrun{
+#' # from character string
 #' view(lawn_data$polygons_average)
 #' view(lawn_data$filter_features)
 #' view(lawn_data$polygons_within)
 #' view(lawn_data$polygons_count)
+#'
+#' # from a list
+#' library("jsonlite")
+#' mylist <- fromJSON(lawn_data$polygons_average, FALSE)
+#' view(mylist)
 #' }
 view <- function(x) {
+  UseMethod("view")
+}
+
+#' @export
+view.character <- function(x) {
+  make_view(x)
+}
+
+#' @export
+view.list <- function(x) {
+  make_view(toJSON(x, auto_unbox = TRUE))
+}
+
+make_view <- function(x) {
   b <- as.list(setNames(extent(x), c("lng1","lat1","lng2","lat2")))
   leaflet() %>%
     addTiles() %>%
