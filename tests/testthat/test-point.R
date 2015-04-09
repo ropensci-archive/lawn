a88b48ae17198fe2ea6afed238400a7eecaa5ba2
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-08 17:27:12 -0700

    added tests for point and polygon methods

---
+++ tests/testthat/test-point.R
@@ -0,0 +1,23 @@
+context("point")
+
+test_that("point works", {
+  a <- point(c(40, -74.50))
+  b <- point(c(40, -74.50), properties = list(name = 'poly1', population = 400))
+
+  expect_is(a, "list")
+  expect_equal(a$type, "Feature")
+  expect_is(a$geometry, "list")
+  expect_equal(length(a$properties), 0)
+
+  expect_is(b, "list")
+  expect_is(b$geometry, "list")
+  expect_is(b$properties, "list")
+  expect_equal(b$type, "Feature")
+  expect_equal(length(b$properties), 2)
+})
+
+test_that("point fails correctly", {
+  expect_error(point(), "argument \"x\" is missing, with no default")
+  expect_error(point(x = 5), "Coordinates must be at least 2 numbers long")
+  expect_error(point(NULL), "Coordinates must be an array")
+})
---
+++ tests/testthat/test-polygon.R
@@ -0,0 +1,35 @@
+context("polygon")
+
+test_that("polygon works", {
+  rings <- list(list(
+    c(-2.275543, 53.464547),
+    c(-2.275543, 53.489271),
+    c(-2.215118, 53.489271),
+    c(-2.215118, 53.464547),
+    c(-2.275543, 53.464547)
+  ))
+  a <- polygon(rings)
+  b <- polygon(rings, properties = list(name = 'poly1', population = 400))
+
+  expect_is(a, "list")
+  expect_equal(a$type, "Feature")
+  expect_is(a$geometry, "list")
+  expect_is(a$geometry$coordinates, "array")
+  expect_equal(length(a$geometry$coordinates), 10)
+  expect_equal(length(a$properties), 0)
+
+  expect_is(b, "list")
+  expect_equal(b$type, "Feature")
+  expect_is(b$geometry, "list")
+  expect_is(b$properties, "list")
+  expect_equal(length(b$properties), 2)
+
+  # passing NULL gives empty polygon geometry
+  expect_is(polygon(NULL), "list")
+})
+
+test_that("polygon fails correctly", {
+  expect_error(polygon(), "argument \"rings\" is missing, with no default")
+  expect_error(polygon(5), "Cannot read property")
+  expect_error(polygon("stuff"), "First and last Position are not equivalent")
+})
