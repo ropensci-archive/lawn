eff87f6301c4a10168d43f75736baa6cf6e95aae
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-09 13:39:29 -0700

    fixed polygons test

--- tests/testthat/test-polygon.R
+++ tests/testthat/test-polygon.R
@@ -11,21 +11,23 @@ test_that("polygon works", {
   a <- polygon(rings)
   b <- polygon(rings, properties = list(name = 'poly1', population = 400))
 
-  expect_is(a, "list")
+  expect_is(a, "polygon")
+  expect_is(unclass(a), "list")
   expect_equal(a$type, "Feature")
   expect_is(a$geometry, "list")
   expect_is(a$geometry$coordinates, "array")
   expect_equal(length(a$geometry$coordinates), 10)
   expect_equal(length(a$properties), 0)
 
-  expect_is(b, "list")
+  expect_is(b, "polygon")
+  expect_is(unclass(b), "list")
   expect_equal(b$type, "Feature")
   expect_is(b$geometry, "list")
   expect_is(b$properties, "list")
   expect_equal(length(b$properties), 2)
 
   # passing NULL gives empty polygon geometry
-  expect_is(polygon(NULL), "list")
+  expect_is(polygon(NULL), "polygon")
 })
 
 test_that("polygon fails correctly", {
