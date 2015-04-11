80d1530ec08f555a3e4bc56aabde8ad9be3b4afe
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-10 19:32:34 -0700

    added inside fxn

---
+++ R/inside.R
@@ -0,0 +1,55 @@
+#' Inside
+#'
+#' Takes a Point and a Polygon or MultiPolygon and determines if the point
+#' resides inside the polygon. The polygon can be convex or concave. The
+#' function accounts for holes
+#'
+#' @export
+#'
+#' @param point Input point
+#' @param polygon Input polygon or multipolygon
+#' @return \code{TRUE} if the Point IS inside the Polygon, \code{FALSE} if
+#' the Point IS NOT inside the Polygon
+#' @examples
+#' point1 <- '{
+#'   "type": "Feature",
+#'   "properties": {
+#'     "marker-color": "#f00"
+#'   },
+#'   "geometry": {
+#'     "type": "Point",
+#'     "coordinates": [-111.467285, 40.75766]
+#'   }
+#' }'
+#' point2 <- '{
+#'   "type": "Feature",
+#'   "properties": {
+#'     "marker-color": "#0f0"
+#'   },
+#'   "geometry": {
+#'     "type": "Point",
+#'     "coordinates": [-111.873779, 40.647303]
+#'   }
+#' }'
+#' poly <- '{
+#'   "type": "Feature",
+#'   "properties": {},
+#'   "geometry": {
+#'     "type": "Polygon",
+#'     "coordinates": [[
+#'       [-112.074279, 40.52215],
+#'       [-112.074279, 40.853293],
+#'       [-111.610107, 40.853293],
+#'       [-111.610107, 40.52215],
+#'       [-112.074279, 40.52215]
+#'       ]]
+#'   }
+#' }'
+#' inside(point1, poly)
+#' inside(point2, poly)
+inside <- function(point, polygon) {
+  point <- convert(point)
+  polygon <- convert(polygon)
+  ct$eval(sprintf("var ins = turf.inside(%s, %s);", point, polygon))
+  ct$get("ins")
+}
