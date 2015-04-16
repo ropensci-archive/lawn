a9709b17b2d5c8e31563eb6715331a2f83ce6ee4
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-16 13:23:18 -0700

    added a test for the fli pfxn

---
+++ tests/testthat/test-flip.R
@@ -0,0 +1,36 @@
+context("flip")
+
+library("jsonlite")
+
+a <- flip(lawn_data$points_average)
+b <- flip(lawn_data$polygons_average)
+
+test_that("flip works", {
+  expect_is(a, "list")
+  expect_is(a$type, "character")
+  expect_is(a$features, "data.frame")
+  expect_is(a$features$geometry$type, "character")
+  expect_is(a$features$geometry$coordinates[[1]], "numeric")
+
+  pts1 <- fromJSON(lawn_data$points_average)$features$geometry$coordinates[[1]]
+  apts1 <- a$features$geometry$coordinates[[1]]
+  expect_equal(apts1[1], pts1[2])
+})
+
+test_that("flip fails correctly", {
+  expect_equal(flip(5, FALSE), 5)
+  expect_error(flip(5, TRUE), "Line 0 - The root of a GeoJSON object must be an object")
+  expect_error(flip("adfdfasds"), "ReferenceError")
+
+  bad1 <- '{"type":"Feature","properties":{},"geometry":{"type":"point","coordinates":[20.566406,43.421008]}}'
+  expect_error(flip(bad1, TRUE), "The type point is unknown")
+
+  bad2 <- '{"type":"Feature","properties":{},"geometry":{"type":"Point","cordinates":[20.566406,43.421008]}}'
+  expect_error(flip(bad2, TRUE), '"coordinates" property required')
+
+  bad3 <- '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[20.566406 43.421008]}}'
+  expect_error(flip(bad3, TRUE), 'parse error')
+
+  bad4 <- '{"typ":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[20.566406, 43.421008]}}'
+  expect_error(flip(bad4, TRUE), 'The type property is required and was not found')
+})
