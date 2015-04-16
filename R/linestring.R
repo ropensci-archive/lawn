7e2ed0852908a33fedc328dfb4d9a0ef0db22c18
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-16 09:47:57 -0700

    changed linestring internals to allow json or R object input

--- R/linestring.R
+++ R/linestring.R
@@ -4,6 +4,21 @@
 #' @param coordinates A list of Positions
 #' @param properties A list of properties
 #' @examples
+#' linestring1 <- '[
+#'    [-21.964416, 64.148203],
+#'    [-21.956176, 64.141316],
+#'    [-21.93901, 64.135924],
+#'    [-21.927337, 64.136673]
+#' ]'
+#' linestring2 <- '[
+#'    [-21.929054, 64.127985],
+#'    [-21.912918, 64.134726],
+#'    [-21.916007, 64.141016],
+#'    [-21.930084, 64.14446]
+#' ]'
+#' linestring(linestring1)
+#' linestring(linestring2)
+#'
 #' pts <- list(
 #'    c(-21.964416, 64.148203),
 #'    c(-21.956176, 64.141316),
@@ -17,6 +32,6 @@
 #' linestring(pts)
 
 linestring <- function(coordinates, properties = NULL) {
-  ct$eval(sprintf("var poly = turf.linestring(%s, %s);", toj(coordinates), toj(properties)))
+  ct$eval(sprintf("var poly = turf.linestring(%s, %s);", convert(coordinates), toj(properties)))
   structure(ct$get("poly"), class = "linestring")
 }
