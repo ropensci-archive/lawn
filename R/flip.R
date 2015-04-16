5eef81eb0b3b62205c5d01c44965be922701785f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-15 23:14:04 -0700

    added flip fxn

---
+++ R/flip.R
@@ -0,0 +1,25 @@
+#' Flip x,y to y,x, and vice versa
+#'
+#' @export
+#' @param input Feature of features
+#' @examples
+#' serbia <- '{
+#'   "type": "Feature",
+#'   "properties": {},
+#'   "geometry": {
+#'     "type": "Point",
+#'     "coordinates": [20.566406, 43.421008]
+#'    }
+#' }'
+#' flip(serbia)
+#' @examples \dontrun{
+#' lawn_data$points_average %>% view
+#' flip(lawn_data$points_average) %>% view
+#' lawn_data$polygons_average %>% view
+#' flip(lawn_data$polygons_average) %>% view
+#' }
+
+flip <- function(input) {
+  ct$eval(sprintf("var flp = turf.flip(%s);", convert(input)))
+  ct$get("flp")
+}
