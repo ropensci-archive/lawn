10403404f0ade21f7ce158155fa3d455c625ef42
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-16 13:11:03 -0700

    added geojsonhint for linting

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,10 +1,14 @@
 #' @importFrom V8 new_context
 ct <- NULL
 rand <- NULL
+ht <- NULL
 .onLoad <- function(libname, pkgname){
   ct <<- new_context();
   ct$source(system.file("js/turf.js", package = pkgname))
 
   rand <<- new_context();
   rand$source(system.file("js/geojson_random.js", package = pkgname))
+
+  ht <<- new_context();
+  ht$source(system.file("js/geojsonhint.js", package = pkgname))
 }
