5cd0f8f029f999a7894c4173c29576c0958f77bd
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-16 18:08:55 -0700

    added tests for line_slice and point_on_line

---
+++ tests/testthat/test-line_slice.R
@@ -0,0 +1,48 @@
+context("line_slice")
+
+start <- '{
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-77.029609, 38.881946]
+  }
+}'
+stop <- '{
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-77.021884, 38.889563]
+  }
+}'
+line <- '{
+ "type": "Feature",
+ "properties": {},
+ "geometry": {
+   "type": "LineString",
+   "coordinates": [
+     [-77.031669, 38.878605],
+     [-77.029609, 38.881946],
+     [-77.020339, 38.884084],
+     [-77.025661, 38.885821],
+     [-77.021884, 38.889563],
+     [-77.019824, 38.892368]
+    ]
+  }
+}'
+
+a <- line_slice(start, stop, line)
+
+test_that("line_slice works", {
+  expect_is(a, "list")
+  expect_is(a$type, "character")
+  expect_is(a$geometry$coordinates, "matrix")
+  expect_is(a$geometry$type, "character")
+  expect_is(a$geometry$coordinates[[1]], "numeric")
+})
+
+test_that("line_slice fails correctly", {
+  expect_error(line_slice(), "argument \"point1\" is missing, with no default")
+  expect_error(line_slice(5, 5, 6, TRUE), "Line 0 - The root of a GeoJSON object must be an object")
+})
---
+++ tests/testthat/test-point_on_line.R
@@ -0,0 +1,39 @@
+context("point_on_line")
+
+line <- '{
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "LineString",
+    "coordinates": [
+      [-77.031669, 38.878605],
+      [-77.029609, 38.881946],
+      [-77.020339, 38.884084],
+      [-77.025661, 38.885821],
+      [-77.021884, 38.889563],
+      [-77.019824, 38.892368]
+    ]
+  }
+}'
+pt <- '{
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-77.037076, 38.884017]
+  }
+}'
+a <- point_on_line(line, pt)
+
+test_that("point_on_line works", {
+  expect_is(a, "list")
+  expect_is(a$type, "character")
+  expect_is(a$geometry, "list")
+  expect_is(a$geometry$type, "character")
+  expect_is(a$geometry$coordinates, "numeric")
+})
+
+test_that("point_on_line fails correctly", {
+  expect_error(point_on_line(), "argument \"line\" is missing, with no default")
+  expect_error(point_on_line(5, TRUE), "input must be a LineString Feature or Geometry")
+})
