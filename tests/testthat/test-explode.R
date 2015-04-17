d486b47af7692fadb89e8d9366d1b2f4e6b7367f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-16 17:08:10 -0700

    added explode and kinks tests

---
+++ tests/testthat/test-explode.R
@@ -0,0 +1,34 @@
+context("explode")
+
+poly <- '{
+ "type": "Feature",
+ "properties": {},
+ "geometry": {
+   "type": "Polygon",
+   "coordinates": [[
+     [177.434692, -17.77517],
+     [177.402076, -17.779093],
+     [177.38079, -17.803937],
+     [177.40242, -17.826164],
+     [177.438468, -17.824857],
+     [177.454948, -17.796746],
+     [177.434692, -17.77517]
+    ]]
+ }
+}'
+a <- explode(poly)
+
+test_that("explode works", {
+  expect_is(a, "list")
+  expect_is(a$type, "character")
+  expect_is(a$features, "data.frame")
+  expect_is(a$features$geometry$type, "character")
+  expect_is(a$features$geometry$coordinates[[1]], "numeric")
+  expect_equal(a$features$geometry$type[1], "Point")
+})
+
+test_that("explode fails correctly", {
+  expect_error(explode(), "argument \"input\" is missing, with no default")
+  expect_error(explode(5), "Unknown Geometry Type")
+  expect_error(explode(5, TRUE), "Line 0 - The root of a GeoJSON object must be an object")
+})
---
+++ tests/testthat/test-kinks.R
@@ -0,0 +1,39 @@
+context("kinks")
+
+poly <- '{
+ "type": "Feature",
+ "properties": {},
+ "geometry": {
+   "type": "Polygon",
+   "coordinates": [[
+     [-12.034835, 8.901183],
+     [-12.060413, 8.899826],
+     [-12.03638, 8.873199],
+     [-12.059383, 8.871418],
+     [-12.034835, 8.901183]
+    ]]
+  }
+}'
+a <- kinks(poly)
+
+test_that("kinks works", {
+  expect_is(a, "list")
+  expect_is(a$intersections, "list")
+  expect_null(a$fixed)
+  expect_is(a$intersections$features, "data.frame")
+  expect_is(a$intersections$features$type, "character")
+  expect_is(a$intersections$features$geometry, "data.frame")
+  expect_equivalent(round(a$intersections$features$geometry$coordinates[[1]][1]), -12)
+})
+
+test_that("kinks fails correctly", {
+  expect_error(kinks(), "argument \"input\" is missing, with no default")
+  expect_error(kinks(5, TRUE), "Line 0 - The root of a GeoJSON object must be an object")
+  expect_error(kinks("adfadfs"), "ReferenceError")
+
+  bad1 <- '{"type":"Featureadf","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-12.034835,8.901183],[-12.060413,8.899826],[-12.03638,8.873199],[-12.059383,8.871418],[-12.034835,8.901183]]]}}'
+  expect_error(kinks(bad1, TRUE), "The type Featureadf is unknown")
+
+  bad2 <- '{"type":"Feature","properties":{},"geometry":{"type":"Polygan","coordinates":[[[-12.034835,8.901183],[-12.060413,8.899826],[-12.03638,8.873199],[-12.059383,8.871418],[-12.034835,8.901183]]]}}'
+  expect_error(kinks(bad2, TRUE), "The type Polygan is unknown")
+})
