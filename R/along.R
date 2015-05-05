7a11b09c283d44bc06be4d1ca8ef70a282ff888f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-04 18:38:36 -0700

    fixes to along fxn and fix to link to data type man file

--- R/along.R
+++ R/along.R
@@ -6,32 +6,24 @@
 #' @param units	can be degrees, radians, miles (default), or kilometers
 #' @template lint
 #' @return \code{\link{data-Point}} distance units along the line
-#' @details Takes a \code{\link{data-Linestring}} and returns a \code{\link{data-Point}}
+#' @details Takes a \code{\link{data-LineString}} and returns a \code{\link{data-Point}}
 #' at a specified distance along the line.
 #' @examples
-#' line <- '{
-#' "type": "Feature",
-#' "properties": {},
-#' "geometry": {
-#'   "type": "LineString",
-#'   "coordinates": [
-#'     [-77.031669, 38.878605],
-#'     [-77.029609, 38.881946],
-#'     [-77.020339, 38.884084],
-#'     [-77.025661, 38.885821],
-#'     [-77.021884, 38.889563],
-#'     [-77.019824, 38.892368]
-#'     ]
-#' }
-#' }'
-#' along(line, 1, 'miles')
+#' pts <- '[
+#'    [-21.964416, 64.148203],
+#'    [-21.956176, 64.141316],
+#'    [-21.93901, 64.135924],
+#'    [-21.927337, 64.136673]
+#' ]'
+#' along(linestring(pts), 1, 'miles')
 #' @examples \dontrun{
 #' along(line, 1, 'miles') %>% view
 #' res <- along(line, 1, 'miles')
 #' featurecollection(list(linestring(line), res)) %>% view
 #' }
 along <- function(line, distance, units, lint = FALSE) {
+  line <- convert(line)
   lawnlint(line, lint)
-  ct$eval(sprintf("var alg = turf.along(%s, %s, '%s');", convert(line), distance, units))
+  ct$eval(sprintf("var alg = turf.along(%s, %s, '%s');", line, distance, units))
   structure(ct$get("alg"), class = "point")
 }
