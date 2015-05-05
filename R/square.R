3f9a530f1ee3ea711b4292f0ffb437806d6942ba
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-04 23:35:30 -0700

    added square fxn

---
+++ R/square.R
@@ -0,0 +1,18 @@
+#' Square
+#'
+#' @export
+#' @param bbox A bounding box
+#' @template lint
+#' @return A square surrounding bbox
+#' @details Takes a bounding box and calculates the minimum square bounding
+#' box that would contain the input.
+#' @examples
+#' bbox <- c(-20, -20, -15, 0)
+#' square(bbox)
+#' square(bbox, TRUE)
+#' sq <- square(bbox)
+#' featurecollection(list(bbox_polygon(bbox), bbox_polygon(sq))) %>% view
+square <- function(bbox, lint = FALSE) {
+  ct$eval(sprintf("var sq = turf.square(%s);", convert(bbox)))
+  ct$get("sq")
+}
