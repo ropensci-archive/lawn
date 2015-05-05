5b28127ae914858b771c2b1d7d301e556579c9fc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-04 19:42:47 -0700

    added erase fxn

---
+++ R/erase.R
@@ -0,0 +1,57 @@
+#' Erase
+#'
+#' @export
+#' @param poly1 input Polygon feaure
+#' @param poly2 Polygon feature to erase from poly1
+#' @template lint
+#' @return a \code{\link{data-Polygon}} feature showing the area of poly1 excluding
+#' the area of poly2
+#' @details Finds the difference between two \code{\link{data-Polygon}} by clipping
+#' the second polygon from the first.
+#' @examples
+#' poly1 <- '{
+#'  "type": "Feature",
+#'   "properties": {
+#'     "fill": "#0f0"
+#'   },
+#'   "geometry": {
+#'     "type": "Polygon",
+#'     "coordinates": [[
+#'       [-46.738586, -23.596711],
+#'       [-46.738586, -23.458207],
+#'       [-46.560058, -23.458207],
+#'       [-46.560058, -23.596711],
+#'       [-46.738586, -23.596711]
+#'     ]]
+#'   }
+#' }'
+#'
+#' poly2 <- '{
+#'   "type": "Feature",
+#'   "properties": {
+#'     "fill": "#00f"
+#'   },
+#'   "geometry": {
+#'     "type": "Polygon",
+#'     "coordinates": [[
+#'       [-46.650009, -23.631314],
+#'       [-46.650009, -23.5237],
+#'       [-46.509246, -23.5237],
+#'       [-46.509246, -23.631314],
+#'       [-46.650009, -23.631314]
+#'     ]]
+#'   }
+#' }'
+#' erase(poly1, poly2)
+#' @examples \dontrun{
+#' erase(poly1, poly2) %>% view
+#' featurecollection(list(poly1, poly2)) %>% view
+#' # featurecollection(list(polygon(poly1), polygon(poly2))) %>% view
+#' }
+erase <- function(poly1, poly2, lint = FALSE) {
+  poly1 <- convert(poly1)
+  poly2 <- convert(poly2)
+  lawnlint(list(poly1, poly2), lint)
+  ct$eval(sprintf("var er = turf.erase(%s, %s);", poly1, poly2))
+  structure(ct$get("er"), class = "polygon")
+}
