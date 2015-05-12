923cd0616a3f16463a5045920357fc5fba485568
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-11 20:21:34 -0700

    updated man files for math fxns

--- man-roxygen/math.R
+++ man-roxygen/math.R
@@ -1,4 +1,4 @@
 #' @param polygons a FeatureCollection of \code{\link{data-Polygon}} features
 #' @param points a FeatureCollection of \code{\link{data-Point}} features
-#' @param in_field the field in input data to analyze
-#' @param out_field the field in which to store results
+#' @param in_field (character) the field in input data to analyze
+#' @param out_field (character) the field in which to store results
