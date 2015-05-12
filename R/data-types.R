d0e1970049dd989fea38fc8b20611e592a3a649a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-12 15:08:00 -0700

    updated data-types man file

--- R/data-types.R
+++ R/data-types.R
@@ -21,13 +21,19 @@
 #'
 #' For type "Point", the "coordinates" member must be a single position.
 #'
-#' An example: \code{{ "type": "Point", "coordinates": [100.0, 0.0] }}
+#' Example JSON: \code{{ "type": "Point", "coordinates": [100.0, 0.0] }}
+#'
+#' In \code{lawn}: \code{lawn_point(c(1, 2))}
+#'
+#' See: \code{\link{lawn_point}}
 #'
 #' @section MultiPoint:
 #'
 #' For type "MultiPoint", the "coordinates" member must be an array of positions.
 #'
-#' An example: \code{{ "type": "MultiPoint", "coordinates": [ [100.0, 0.0], [101.0, 1.0] ] }}
+#' Example JSON: \code{{ "type": "MultiPoint", "coordinates": [ [100.0, 0.0], [101.0, 1.0] ] }}
+#'
+#' Creation of multipoint in \code{lawn} coming soon
 #'
 #' @section Polygon:
 #'
@@ -35,22 +41,28 @@
 #' coordinate arrays. For Polygons with multiple rings, the first must be the
 #' exterior ring and any others must be interior rings or holes.
 #'
-#' An example:
+#' Example JSON:
 #' \code{{ "type": "Polygon", "coordinates": [
 #'  [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ]] }}
+#'
+#' In \code{lawn}: \code{lawn_polygon(list(list(c(-2, 52), c(-3, 54), c(-2, 53), c(-2, 52))))}
+#'
+#' See: \code{\link{lawn_polygon}}
 #'
 #' @section MultiPolygon:
 #'
 #' For type "MultiPolygon", the "coordinates" member must be an array of
 #' Polygon coordinate arrays.
 #'
-#' An example:
+#' Example JSON:
 #' \code{{ "type": "MultiPolygon", "coordinates": [
 #'   [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
 #'   [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
 #'   [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]
 #'   ]
 #' }}
+#'
+#' Creation of multipolygon in \code{lawn} coming soon
 #'
 #' @section LineString:
 #'
@@ -60,13 +72,28 @@
 #' LinearRing is not explicitly represented as a GeoJSON geometry type, it is
 #' referred to in the Polygon geometry type definition.
 #'
-#' An example:
+#' Example JSON:
 #' \code{{ "type": "LineString", "coordinates": [ [100.0, 0.0], [101.0, 1.0] ] }}
+#'
+#' In \code{lawn}: \code{lawn_linestring(list(c(-2, 52), c(-3, 54), c(-2, 53)))}
+#'
+#' See: \code{\link{lawn_linestring}}
 #'
 #' @section MultiLineString:
 #'
 #' For type "MultiLineString", the "coordinates" member must be an array of
 #' LineString coordinate arrays.
+#'
+#' Example JSON:
+#' \code{{
+#'  "type": "MultiLineString",
+#'  "coordinates": [
+#'      [[ -105, 39 ], [ -105, 39 ]],
+#'      [[ -105, 39 ], [ -105, 39 ]]
+#'   ]
+#' }}
+#'
+#' Creation of multilinestring in \code{lawn} coming soon
 #'
 #' @section Feature:
 #'
@@ -89,16 +116,29 @@
 #' "features". The value corresponding to "features" is an array. Each element in
 #' the array is a feature object as defined above.
 #'
+#' In \code{lawn}: \code{lawn_featurecollection(lawn_point(c(-75, 39)))}
+#'
+#' See: \code{\link{lawn_featurecollection}}
+#'
 #' @section GeometryCollection:
 #'
 #' Each element in the geometries array of a GeometryCollection is one of the geometry
 #' objects described above.
 #'
-#' An example:
-#' \code{
-#' { "type": "GeometryCollection", "geometries": [ { "type": "Point",
-#' "coordinates": [100.0, 0.0] }, { "type": "LineString", "coordinates":
-#' [ [101.0, 0.0], [102.0, 1.0] ] } ] }}
+#' Example JSON:
+#' \code{{
+#'    "type": "GeometryCollection",
+#'    "geometries": [ {
+#'      "type": "Point",
+#'      "coordinates": [100.0, 0.0]
+#'    }, {
+#'    "type": "LineString",
+#'    "coordinates": [ [101.0, 0.0], [102.0, 1.0] ]
+#'    }
+#'  ]
+#' }}
+#'
+#' Creation of geometrycollection in \code{lawn} coming soon
 #'
 #' @name data-types
 #' @aliases data-Point data-MultiPoint data-Polygon data-MultiPolygon data-LineString
