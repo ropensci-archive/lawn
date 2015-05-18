e717a02c211af4a39348603a0d1179cec42c90d2
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-18 12:36:51 -0700

    R 3.1 requires use of ls() instead of names() to list an environment.

--- tests/testthat/test-onload.R
+++ tests/testthat/test-onload.R
@@ -5,18 +5,18 @@ obj_names <- c('eval', 'assign', 'validate', 'call',
 
 test_that("onload for turf.js worked", {
   expect_is(ct, "V8")
-  expect_named(ct, obj_names)
+  expect_true(all(obj_names %in% ls(ct)))
   expect_true(grepl("function", ct$eval("turf.min")))
 })
 
 test_that("onload for geojson-random worked", {
   expect_is(rand, "V8")
-  expect_named(rand, obj_names)
+  expect_true(all(obj_names %in% ls(rand)))
   expect_true(grepl("function", rand$eval("georandom.point")))
 })
 
 test_that("onload for geojsonhint worked", {
   expect_is(ht, "V8")
-  expect_named(ht, obj_names)
+  expect_true(all(obj_names %in% ls(ht)))
   expect_true(grepl("function", ht$eval("geojsonhint.hint")))
 })
