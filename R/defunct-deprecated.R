86e28975f3781db36b15c9e2126d78e81ef515d9
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-17 15:29:09 -0700

    removed aggregate, added collect method and test for it bumped dev version

--- R/defunct-deprecated.R
+++ R/defunct-deprecated.R
@@ -34,6 +34,14 @@ lawn_quantile <- function(...) {
   .Defunct(msg = "lawn_quantile is defunct - it has been removed from turf.js")
 }
 
+#' Calculate a series of aggregations for a set of points within polygons
+#'
+#' This function is defunct
+#'
+#' @keywords internal
+lawn_aggregate <- function(...) {
+  .Defunct(msg = "lawn_aggregate is defunct - it has been removed from turf.js")
+}
 
 
 #' Defunct functions in \pkg{lawn}
@@ -45,10 +53,13 @@ lawn_quantile <- function(...) {
 #'  \item \code{\link{lawn_reclass}}: Function removed. The \code{reclass}
 #'  method in turf.js has been removed.
 #'  See \url{https://github.com/Turfjs/turf/issues/306}
-#'  \item \code{\link{lawn_jenks}}: Function removed. The \code{reclass}
+#'  \item \code{\link{lawn_jenks}}: Function removed. The \code{jenks}
 #'  method in turf.js has been removed.
 #'  See \url{https://github.com/Turfjs/turf/issues/306}
-#'  \item \code{\link{lawn_quantile}}: Function removed. The \code{reclass}
+#'  \item \code{\link{lawn_quantile}}: Function removed. The \code{quantile}
+#'  method in turf.js has been removed.
+#'  See \url{https://github.com/Turfjs/turf/issues/306}
+#'  \item \code{\link{lawn_aggregate}}: Function removed. The \code{aggregate}
 #'  method in turf.js has been removed.
 #'  See \url{https://github.com/Turfjs/turf/issues/306}
 #' }
