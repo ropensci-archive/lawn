d031c1bcf6a7a772df45124caf42ba81209d545a
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-17 16:02:05 -0700

    fixed example in lawn_tag to actually work, and fixed tsts, fix #30

--- R/tag.R
+++ R/tag.R
@@ -5,27 +5,30 @@
 #'
 #' @export
 #' @param points Input \code{\link{data-Point}}
-#' @param polygons Input \code{\link{data-Polygon}} or \code{\link{data-MultiPolygon}}
-#' @param poly_id property in polygons to add to joined Point features
-#' @param containing_polyid property in points in which to store joined
+#' @param polygons Input \code{\link{data-Polygon}} or
+#' \code{\link{data-MultiPolygon}}
+#' @param field property in polygons to add to joined Point features
+#' @param out_field property in points in which to store joined
 #' property from polygons
 #' @template lint
 #' @family joins
 #' @return points with containing_polyid property containing values from poly_id
 #' @examples
-#' pts <- lawn_random(n = 30)
-#' polys <- lawn_triangle_grid(c(-77.3876, 38.7198, -76.9482, 39.0277), 30, 'miles')
-#' polys$features$properties <-
-#'    data.frame(fill = c("#f92", "295"), stroke = 1:2, `fill-opacity` = 2:3)
+#' bbox <- c(0, 0, 10, 10)
+#' pts <- lawn_random(n = 30, bbox = bbox)
+#' polys <- lawn_triangle_grid(bbox, 50, 'miles')
+#' polys$features$properties$fill <- "#f92"
+#' polys$features$properties$stroke <- 0
+#' polys$features$properties$`fill-opacity` <- 1
 #' lawn_tag(pts, polys, 'fill', 'marker-color')
 #' @examples \dontrun{
 #' lawn_tag(pts, polys, 'fill', 'marker-color') %>% view
 #' }
-lawn_tag <- function(points, polygons, poly_id, containing_polyid, lint = FALSE) {
+lawn_tag <- function(points, polygons, field, out_field, lint = FALSE) {
   points <- convert(points)
   polygons <- convert(polygons)
   lawnlint(list(points, polygons), lint)
-  ct$eval(sprintf("var tag = turf.tag(%s, %s, '%s', '%s');",
-                  points, polygons, poly_id, containing_polyid))
-  as.fc(ct$get("tag"))
+  ct$eval(sprintf("var xx = turf.tag(%s, %s, '%s', '%s');",
+                  points, polygons, field, out_field))
+  as.fc(ct$get("xx"))
 }
