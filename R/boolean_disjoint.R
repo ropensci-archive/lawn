87a4a2c21a7a4c60fb50ad4b19f6a1e938a531c8
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-14 16:55:07 -0700

    boolean disjoint eg fix

--- R/boolean_disjoint.R
+++ R/boolean_disjoint.R
@@ -7,7 +7,7 @@
 #' @examples
 #' pt1 <- '[2, 2]'
 #' l1 <- '[[1, 1], [1, 2], [1, 3], [1, 4]]'
-#' lawn_boolean_disjoint(lawn_point(pt1), lawn_linestring(l2))
+#' lawn_boolean_disjoint(lawn_point(pt1), lawn_linestring(l1))
 lawn_boolean_disjoint <- function(feature1, feature2, lint = FALSE) {
   lawnlint(list(feature1, feature2), lint)
   ct$eval(sprintf("var bdj = turf.booleanDisjoint(%s, %s);",
