8ed0b02e903c7d69651c635a52a22943a492067c
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-14 17:02:33 -0700

    fix #73 pointgrid changes

--- R/point_grid.R
+++ R/point_grid.R
@@ -6,17 +6,25 @@
 #' @export
 #'
 #' @param extent (numeric) Extent in `[minX, minY, maxX, maxY]` order.
-#' @param cellWidth (integer) Width of each cell.
+#' @param cellSide (integer) the distance between points
 #' @param units (character) Units to use for cellWidth, one of 'miles' or
-#' 'kilometers'.
+#' 'kilometers' (default).
+#' @param centered (logical) adjust points position to center the grid into
+#' bbox. This parameter is going to be removed in the next major release,
+#' having the output always centered into bbox. Default: `TRUE`
+#' @param bboxIsMask if `TRUE`, and bbox is a Polygon or MultiPolygon, the
+#' grid Point will be created only if inside the bbox Polygon(s).
+#' Default: `FALSE`
 #' @family interpolation
 #' @return [data-FeatureCollection] grid of points.
 #' @examples
 #' lawn_point_grid(c(-77.3876, 38.7198, -76.9482, 39.0277), 30, 'miles')
 #' lawn_point_grid(c(-77.3876, 38.7198, -76.9482, 39.0277), 10, 'miles')
 #' lawn_point_grid(c(-77.3876, 38.7198, -76.9482, 39.0277), 3, 'miles')
-lawn_point_grid <- function(extent, cellWidth, units) {
-  ct$eval(sprintf("var pg = turf.pointGrid(%s, %s, '%s');", toj(extent),
-                  cellWidth, units))
+lawn_point_grid <- function(extent, cellSide, units = "kilometers",
+                            centered = TRUE, bboxIsMask = FALSE) {
+
+  ct$eval(sprintf("var pg = turf.pointGrid(%s, %s, '%s', %s, %s);", toj(extent),
+                  cellSide, units, convert(centered), convert(bboxIsMask)))
   as.fc(ct$get("pg"))
 }
